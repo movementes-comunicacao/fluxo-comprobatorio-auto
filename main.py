102a82eae4d6c77cddc81d1656085879610cf99c
gabrielsbf <gabrielsbfernandes@gmail.com>
2024-06-26 19:12:01 -0300

    Process of input completed -> proccess is working with created sheets, need to make a proccess to new months that aren't already created.

--- main.py
+++ main.py
@@ -9,21 +9,27 @@ import pandas as pd
 
 print(ACCOUNT, CONFIG_INI_PATH)
 
-sh_man = Sheets_Manager(SHEET_URL, SERVICE_ACC)
-social_man = Social_Manager(ACCOUNT, CONFIG_INI_PATH, './data')
-twitter_man = Twitter_Manager('NiteroiPref', BROWSER_DATA_PATH, USER_AGENT, False)
+
 if __name__ == "__main__":
+	sh_man = Sheets_Manager(SHEET_URL, SERVICE_ACC)
+	social_man = Social_Manager(ACCOUNT, CONFIG_INI_PATH, './data')
+	twitter_man = Twitter_Manager('NiteroiPref', BROWSER_DATA_PATH, USER_AGENT, False)
+	dt_man = Date_Utils()
+	
 	sh_config = sh_man.access_sheet('config')
 	last_dt = datetime.strptime(sh_config.get("F15")[0][0], "%d/%m/%Y %H:%M:%S")
-	since = last_dt.replace(day=(last_dt.day)+1, hour=0, minute=0, second=0)
+	since = last_dt.replace(minute=last_dt.minute+10, second=0)
+	# since = datetime(2024, 5, 30)
 	until = datetime.now().replace(day=datetime.now().day-1, hour=23, minute=59, second=59, microsecond=0)
 	social_man.date_optional = [since, until]
 	period = social_man.return_period()
 	if since < until:
 		print("nova solicitação!")
-		result = separate_months_req(merge_posts(
+		# SeparateMonthsByReq precisa vir aqui -> para caso cada mês dê ruim.
+		result = merge_posts(
 			get_face_essencial(social_man, [since, until]),
 			get_insta_essencial(social_man, [since, until]),
 			get_twitter_essencial(twitter_man, [since, until])
-			), period)
-		print(result)
+			)
+		pd.DataFrame(result).to_excel("Relatorio.xlsx")
+		input_on_sheets(sh_man, result, period, dt_man)
--- srcs/auto_sheets/sheet_input.py
+++ srcs/auto_sheets/sheet_input.py
@@ -1,26 +1,15 @@
-import gspread
+from gspread.worksheet import Worksheet
 from components.Sheets_Manager.module.sheets_manager import Sheets_Manager
 from datetime import datetime
-
-def input_on_sheets(sh_man: Sheets_Manager, metrics: list[dict], months:dict):
-	since_prop = months['start_date']
-	until_prop = months['final_date']
-	since_month = str(since_prop.get('mes')).capitalize()[0:3]
-	until_month = str(until_prop.get('mes')).capitalize()[0:3]
-	if since_month == until_month:
-		print("é igual")
-	else:
-		
-		separate_months_req(sh_man, metrics, months)
-	return 'terminei'
-	# return(sh_man.get_line_response('config', until_month, column_search='E', column_response='G'))
-	
+from components.Date_Utils.module.date_time_utils import Date_Utils
 
 def arr_sep_by_month(metrics: list[dict], req_arr: dict[list])->None:
+	"""get the post by post json, format the date string to datetime format and append on the 
+	specific requisitions on a array with the key of the number of the related month."""
 	for post in metrics:
 		date = datetime.strptime(post['date_created'][0:10], "%Y-%m-%d")
 		# Essa linha transforma uma string de data num elemento datetime.
-		post['date_created'] = date
+		# post['date_created'] = date
 		month = date.month
 		req_arr.get(month).append(post)
 
@@ -31,3 +20,26 @@ def separate_months_req(metrics: list[dict], months: dict):
 	result_arr = {since_prop.get('num_mes') + i: [] for i in range (0, period_range)}
 	arr_sep_by_month(metrics, result_arr)
 	return result_arr
+
+def access_or_create_sheet(sh_man: Sheets_Manager, sheet_name=None) -> Worksheet | None:
+	list_sheets = [i.title for i in  sh_man.spreadsheet_obj.worksheets()]
+	if sheet_name in list_sheets:
+		return sh_man.access_sheet(sheet_name)
+	else:
+		print(f"Guia {sheet_name} não localizada, necessita de um processo individual de criação.")
+		return None
+
+
+def input_on_sheets(sh_man: Sheets_Manager, metrics: list[dict], months:dict, date_man: Date_Utils):
+	result_arr = separate_months_req(metrics, months)
+	del metrics
+	for month_num, arr in result_arr.items():
+		sheet_name = date_man.months["portuguese"].get(month_num).capitalize()[0:3]
+		print("sheet name é ", sheet_name)
+		sheet_obj = access_or_create_sheet(sh_man, sheet_name)
+		if sheet_obj == None:
+			continue
+		last_line = float(sh_man.get_line_response("config", str(month_num), "D", "G").replace(",","."))
+		sh_man.input_data(arr, sheet_obj, 'C'+str(int(last_line) + 1),["description"])
+		sh_man.input_data(arr, sheet_obj, 'E'+str(int(last_line) + 1),["link_url"])
+		sh_man.input_data(arr, sheet_obj, 'I'+str(int(last_line) + 1),["date_created"])
