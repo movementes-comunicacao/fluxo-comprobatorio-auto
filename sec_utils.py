a48cf681ed22e76e8bfda160853cb090493e7a50
gabrielsbf <gabrielsbfernandes@gmail.com>
2024-07-17 17:41:44 -0300

    refactor: new folders for the sec functions

--- sec_utils.py
+++ sec_utils.py
@@ -1,6 +1,6 @@
 from components.Driver_Flow.module.drive_auth_p import *
 from utils.read_env import *
-
+from components.Sheets_Manager.module.sheets_manager import *
 def create_month_folder(driver_m: Drive_Manager, month_fd_url:str):
 	"""
 	Create the folders of the days inside an specific url
@@ -13,5 +13,4 @@ def create_month_folder(driver_m: Drive_Manager, month_fd_url:str):
 		driver_m.create_drive_file("folder", month_fd_url, str(i))
 
 drive_m = Drive_Manager(CREDS_PATH + "token_pickle/token.pickle", CREDS_PATH + "oauth_cred.json")
-
-drive_m.getFileList()
+sheet_m = Sheets_Manager("https://docs.google.com/spreadsheets/d/19cG6ldny58xmZIAQ1uQmICMisy7m0gnpeUiC11L2jO0/", SERVICE_ACC, False)
