295ffba46c804550a965ac66560cefb4894ea927
gabrielsbf <gabrielsbfernandes@gmail.com>
2025-04-03 15:58:38 -0300

    feat: all medias are inserted into program

--- outer_data.py
+++ outer_data.py
@@ -1,30 +1,35 @@
 
-from utils.read_env import *
 from components.Sheets_Manager.module.sheets_manager import Sheets_Manager
 from srcs.auto_sheets.social_extract import *
 from srcs.auto_sheets.sheet_input import *
 from components.Thread_Manager.module.scraping_threads import Threads_Manager
-from srcs.utils import *
 from datetime import datetime, timedelta
 import pandas as pd
 
 env_variable_prefix = "nit"
 if __name__ == "__main__":
-
-	# sh_man = Sheets_Manager(SHEET_URL, SERVICE_ACC)
 	social_man = Social_Manager(ACCOUNT, CONFIG_INI_PATH, './data')
-	# twitter_man = Twitter_Manager(TWITTER_ACC, BROWSER_DATA_PATH, USER_AGENT,"Default",other_options=False,disable_graphics=False, remote_connection=False)
+	ttk = None if TIKTOK_ACC == None else Tiktok_Automation(TIKTOK_ACC)
+	ttk.start_browser()
+	ytb = None if YOUTUBE_ACC == None else Youtube_Automation(YOUTUBE_ACC, ttk.browser, ttk.page)
+	
+	
 	dt_man = Date_Utils()
-	last_dt = datetime(2025, 2, 16, 16, 20, 0)
-	since = last_dt 
-	until = datetime.now().replace(hour=23, minute=59, second=59, microsecond=0) - timedelta(days=1)
+	dates = dt_man.return_period()
+	since = dates["start_date"]["value"]
+	until = dates["final_date"]["value"]
 	print("since is: ", since, "and until is: ", until)
+	social_man.date_optional = [since, until]
+	period = social_man.return_period()
 	if since < until:
 		print("nova solicitação!")
 		# SeparateMonthsByReq precisa vir aqui -> para caso cada mês dê ruim.
 		result = merge_posts(
-			get_insta_essencial(social_man, [since, until]),
-			get_face_essencial(social_man, [since, until]),
-			getTwitterAndThreads([since, until])
+			get_tiktok_essencial(ttk, [since, until]),
+			get_youtube_essencial(ytb, [since, until]),
+			# getTwitterAndThreads([since, until]),
+			# get_face_essencial(social_man, [since, until]),
+			# get_insta_essencial(social_man, [since, until]),
 			)
-		pd.DataFrame(result).to_excel("social.xlsx")
+		# ytb.close_browser()
+		pd.DataFrame(result).to_excel("Relatorio.xlsx")
