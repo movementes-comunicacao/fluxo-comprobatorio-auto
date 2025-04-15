43c3e530a3c0ce61d128d2bb7446264721f57597
gabrielsbf <gabrielsbfernandes@gmail.com>
2025-04-15 12:52:08 -0300

    feat: sheet input correction and main process with manual dates implementation was made in file outer_data.py

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ from components.Thread_Manager.module.scraping_threads import Threads_Manager
 from datetime import datetime, timedelta
 import pandas as pd
 from components.Files_Handler.module.file_handler import Files_Handling
+import sys
 
-env_variable_prefix = "marica"
+env_variable_prefix = "nit"
 if __name__ == "__main__":
 	social_man = Social_Manager(ACCOUNT, CONFIG_INI_PATH, './data')
 	ttk = None if TIKTOK_ACC == None else Tiktok_Automation(TIKTOK_ACC)
 	ttk.start_browser()
 	ytb = None if YOUTUBE_ACC == None else Youtube_Automation(YOUTUBE_ACC, ttk.browser, ttk.page)
-	
-	
+	dateOpt = sys.argv
+	print("DATE OPT LEN IS:", len(dateOpt))
 	dt_man = Date_Utils()
-	if SHEET_URL == "None":
+	if SHEET_URL == "None" or len(dateOpt) > 1 and dateOpt[-1] == "dtchoose":
 		dates = dt_man.return_period()
 		since = dates["start_date"]["value"]
 		until = dates["final_date"]["value"]
@@ -34,15 +35,15 @@ if __name__ == "__main__":
 		print("nova solicitação!")
 		# SeparateMonthsByReq precisa vir aqui -> para caso cada mês dê ruim.
 		result = merge_posts(
+			getTwitterAndThreads([since, until]),
+			get_insta_essencial(social_man, [since, until]),
+			get_face_essencial(social_man, [since, until]),
 			get_tiktok_essencial(ttk, [since, until]),
 			get_youtube_essencial(ytb, [since, until]),
-			getTwitterAndThreads([since, until]),
-			get_face_essencial(social_man, [since, until]),
-			get_insta_essencial(social_man, [since, until]),
 			)
-		
-		Files_Handling("./data/").write_file(result, "data_result.json")
-		# ytb.close_browser()
 		pd.DataFrame(result).to_excel("Relatorio.xlsx")
-		if SHEET_URL != "None":
+		if SHEET_URL != "None" and len(dateOpt) == 1:
 			input_on_sheets(sh_man, result, period, dt_man)
+		
+		# Files_Handling("./data/").write_file(result, "data_result.json")
+		# ytb.close_browser()
--- outer_data.py
+++ outer_data.py
@@ -5,6 +5,8 @@ from srcs.auto_sheets.sheet_input import *
 from components.Thread_Manager.module.scraping_threads import Threads_Manager
 from datetime import datetime, timedelta
 import pandas as pd
+from components.Files_Handler.module.file_handler import Files_Handling
+import sys
 
 env_variable_prefix = "nit"
 if __name__ == "__main__":
@@ -12,12 +14,13 @@ if __name__ == "__main__":
 	ttk = None if TIKTOK_ACC == None else Tiktok_Automation(TIKTOK_ACC)
 	ttk.start_browser()
 	ytb = None if YOUTUBE_ACC == None else Youtube_Automation(YOUTUBE_ACC, ttk.browser, ttk.page)
-	
-	
+	dateOpt = sys.argv
+	print("DATE OPT LEN IS:", len(dateOpt))
 	dt_man = Date_Utils()
 	dates = dt_man.return_period()
 	since = dates["start_date"]["value"]
 	until = dates["final_date"]["value"]
+
 	print("since is: ", since, "and until is: ", until)
 	social_man.date_optional = [since, until]
 	period = social_man.return_period()
@@ -25,11 +28,13 @@ if __name__ == "__main__":
 		print("nova solicitação!")
 		# SeparateMonthsByReq precisa vir aqui -> para caso cada mês dê ruim.
 		result = merge_posts(
+			getTwitterAndThreads([since, until]),
+			get_insta_essencial(social_man, [since, until]),
+			get_face_essencial(social_man, [since, until]),
 			get_tiktok_essencial(ttk, [since, until]),
 			get_youtube_essencial(ytb, [since, until]),
-			# getTwitterAndThreads([since, until]),
-			# get_face_essencial(social_man, [since, until]),
-			# get_insta_essencial(social_man, [since, until]),
 			)
-		# ytb.close_browser()
 		pd.DataFrame(result).to_excel("Relatorio.xlsx")
+		
+		# Files_Handling("./data/").write_file(result, "data_result.json")
+		# ytb.close_browser()
