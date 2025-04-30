6d08a208d150d6025e17db4fb13788d71435b341
gabrielsbf <gabrielsbfernandes@gmail.com>
2025-04-30 15:07:39 -0300

    fix: on execution with playwright integration

--- utils/read_env.py
+++ utils/read_env.py
@@ -5,6 +5,7 @@ ACCOUNT=env_variables["ACCOUNT"]
 SERVICE_ACC=env_variables['SERVICE_ACC_PATH']
 USER_AGENT=env_variables['USER_AGENT']
 BROWSER_DATA_PATH=env_variables['BROWSER_DATA_PATH']
+CHROME_EXECUTABLE_PATH=env_variables['CHROME_EXECUTABLE_PATH']
 CREDS_PATH=env_variables["CREDS_PATH"]
 SHEET_URL=env_variables.get("SHEET_URL")
 TWITTER_ACC=env_variables.get("TWITTER_ACC")
